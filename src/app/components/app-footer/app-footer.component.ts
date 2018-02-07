0d26b23cf85d6c65cc052cb283e49ac176d203fa
Development <development@develapps.es>
2018-02-07 08:27:15 +0100

    ** Fixed year in footer

--- src/app/components/app-footer/app-footer.component.ts
+++ src/app/components/app-footer/app-footer.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './app-footer.component.html'
 })
-export class AppFooterComponent { }
+export class AppFooterComponent {
+
+  public getCurrentYear(): string {
+    return moment().format('YYYY');
+  }
+}
